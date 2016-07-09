0ab3e2ef1829e8080bfa8234b14fbab2c4fe4827
Pascal Garber <pascal@jumplink.eu>
2016-07-09 03:24:54 -0400

    Fix og:image full path; Fix ssl conditions

--- classes/Helper.php
+++ classes/Helper.php
@@ -92,11 +92,17 @@ class Helper {
 
     public static function isSSL()
     {
-        if( (!empty( $_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ) || $_SERVER['SERVER_PORT']==443)
+        if( !empty( $_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ) { 
             return true;
+	}
 
-        if( !empty( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' )
+        if( $_SERVER['SERVER_PORT'] == 443 ) {
             return true;
+        }
+
+        if( !empty( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
+            return true;
+        }
 
         return false;
     }
